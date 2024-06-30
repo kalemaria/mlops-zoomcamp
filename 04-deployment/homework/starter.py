2334e30017e3a96c0d4394ec5b3e49929d415abb
Ria Salfer <maria_kalemanov@yahoo.de>
2024-06-30 09:54:14 +0200

    module 04: Q5. refactor and parametrize the script

--- 04-deployment/homework/starter.py
+++ 04-deployment/homework/starter.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pickle
 import pandas as pd
 import os
 
-year = 2023
-month = 3
-taxi_type = 'yellow'
-
-input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet'
-output_dir = 'output'
-output_file = f'{output_dir}/{taxi_type}_tripdata_predictions_{year:04d}-{month:02d}.parquet'
-
+# Constants
 categorical = ['PULocationID', 'DOLocationID']
+
+def load_dv_and_model(filename):
+    with open(filename, 'rb') as f_in:
+        dv, model = pickle.load(f_in)
+        return dv, model
 
 def read_data(filename):
     df = pd.read_parquet(filename)
@@ -27,28 +26,54 @@ def read_data(filename):
     
     return df
 
-with open('model.bin', 'rb') as f_in:
-    dv, model = pickle.load(f_in)
+def transform_data(df, dv):
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    return X_val
 
-df = read_data(input_file)
+def predict(X_val, model):
+    y_pred = model.predict(X_val)
+    print(f"Standard deviation of the predicted durations: {y_pred.std()}")
+    print(f"Mean predicted duration: {y_pred.mean()}")
+    return y_pred
 
-dicts = df[categorical].to_dict(orient='records')
-X_val = dv.transform(dicts)
-y_pred = model.predict(X_val)
-print(f"Standard deviation of the predicted durations: {y_pred.std()}")
+def prepare_output(df, year, month, y_pred):
+    df['ride_id'] = f'{year}/{month}_' + df.index.astype('str')
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = df['ride_id']
+    df_result['predicted_duration'] = y_pred
+    return df_result
 
-# Preparing the output
-df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
-df_result = pd.DataFrame()
-df_result['ride_id'] = df['ride_id']
-df_result['predicted_duration'] = y_pred
-
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-df_result.to_parquet(
-    output_file,
-    engine='pyarrow',
-    compression=None,
-    index=False
+def write_result(df_result, filename):
+    df_result.to_parquet(
+        filename,
+        engine='pyarrow',
+        compression=None,
+        index=False
 )
 # The size of the output file is 68.6M ~66M
+
+@click.command()
+@click.option('--year', required=True, help='Validation year in YYYY format')
+@click.option('--month', required=True, help='Validation month in MM format')
+def run(year, month):
+    # Input / output filenames
+    model_file = 'model.bin'
+    taxi_type = 'yellow'
+    input_file = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year}-{month}.parquet'
+    output_dir = 'output'
+    output_file = f'{taxi_type}_tripdata_predictions_{year}-{month}.parquet'
+
+    dv, model = load_dv_and_model(model_file)
+    df = read_data(input_file)
+    X_val = transform_data(df, dv)
+    y_pred = predict(X_val, model)
+    df_result = prepare_output(df, year, month, y_pred)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    output_path = os.path.join(output_dir, output_file)
+    write_result(df_result, output_path)
+
+if __name__ == '__main__':
+    run()
